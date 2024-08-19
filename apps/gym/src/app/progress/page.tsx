0d634f15d0e4ab83246aeb243a39de9d77ecd266
jackHotch <jhotchkiss2021@gmail.com>
2024-08-19 15:42:56 -0400

    created button sub components

--- apps/gym/src/app/progress/page.tsx
+++ apps/gym/src/app/progress/page.tsx
@@ -1,13 +1,11 @@
 'use client'
 
-import { Button } from '@gymapp/gymui/Button'
 import { Divider } from '@gymapp/gymui/Divider'
 
 const Progress = () => {
   return (
     <>
       <p>hello</p>
-      <Button variant='primary'>ui</Button>
       <Divider variant='primary'>htshsafddsfja</Divider>
     </>
   )
