45ac2ef9166f483d5809234d0be6ff600e3659ae
jackHotch <jhotchkiss2021@gmail.com>
2024-08-22 12:46:47 -0400

    added tests

---
+++ packages/gymui/src/components/Button/Button.test.tsx
@@ -0,0 +1,48 @@
+import { fireEvent, render } from '@testing-library/react'
+import { Button } from './Button'
+
+describe('Button', () => {
+  it('Button.Primary should render successfully', () => {
+    const buttonClick = jest.fn()
+    const { baseElement, getByText } = render(
+      <Button.Primary onClick={buttonClick}>Primary</Button.Primary>
+    )
+    fireEvent.click(getByText('Primary'))
+
+    expect(baseElement).toBeTruthy()
+    expect(buttonClick).toHaveBeenCalled()
+  })
+
+  it('Button.Secondary should render successfully', () => {
+    const buttonClick = jest.fn()
+    const { baseElement, getByText } = render(
+      <Button.Secondary onClick={buttonClick}>Secondary</Button.Secondary>
+    )
+    fireEvent.click(getByText('Secondary'))
+
+    expect(baseElement).toBeTruthy()
+    expect(buttonClick).toHaveBeenCalled()
+  })
+
+  it('Button.Danger should render successfully', () => {
+    const buttonClick = jest.fn()
+    const { baseElement, getByText } = render(
+      <Button.Danger onClick={buttonClick}>Danger</Button.Danger>
+    )
+    fireEvent.click(getByText('Danger'))
+
+    expect(baseElement).toBeTruthy()
+    expect(buttonClick).toHaveBeenCalled()
+  })
+
+  it('Button.Disabled should render successfully', () => {
+    const buttonClick = jest.fn()
+    const { baseElement, getByText } = render(
+      <Button.Disabled onClick={buttonClick}>Disabled</Button.Disabled>
+    )
+    fireEvent.click(getByText('Disabled'))
+
+    expect(baseElement).toBeTruthy()
+    expect(buttonClick).not.toHaveBeenCalled()
+  })
+})
