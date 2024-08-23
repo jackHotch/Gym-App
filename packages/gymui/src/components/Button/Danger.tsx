30444a8b8b7193756a425409ae6b1f3a607f6d01
jackHotch <jhotchkiss2021@gmail.com>
2024-08-23 16:16:59 -0400

    put each sub component in their own files

---
+++ packages/gymui/src/components/Button/Danger.tsx
@@ -0,0 +1,41 @@
+import { ButtonProps, SCALEHOVER, SCALETAP, DURATION } from './Button'
+import styles from './Button.module.css'
+import { motion } from 'framer-motion'
+
+export const Danger = ({
+  children = 'Danger',
+  sx,
+  my,
+  mx,
+  py,
+  px,
+  width,
+  height,
+  ...props
+}: ButtonProps) => (
+  <motion.button
+    {...props}
+    style={{
+      ...sx,
+      marginTop: my,
+      marginBottom: my,
+      marginLeft: mx,
+      marginRight: mx,
+      paddingTop: py,
+      paddingBottom: py,
+      paddingLeft: px,
+      paddingRight: px,
+      width: width,
+      height: height,
+    }}
+    className={styles.danger}
+    whileHover={{
+      scale: SCALEHOVER,
+      boxShadow: 'rgba(150, 150, 150, 0.5) 0px 2px 8px 2px',
+    }}
+    transition={{ duration: DURATION }}
+    whileTap={{ scale: SCALETAP }}
+  >
+    {children}
+  </motion.button>
+)
---
+++ packages/gymui/src/components/Button/Disabled.tsx
@@ -0,0 +1,36 @@
+import { ButtonProps, SCALEHOVER, SCALETAP, DURATION } from './Button'
+import styles from './Button.module.css'
+import { motion } from 'framer-motion'
+
+export const Disabled = ({
+  children = 'Danger',
+  sx,
+  my,
+  mx,
+  py,
+  px,
+  width,
+  height,
+  ...props
+}: ButtonProps) => (
+  <motion.button
+    {...props}
+    disabled
+    style={{
+      ...sx,
+      marginTop: my,
+      marginBottom: my,
+      marginLeft: mx,
+      marginRight: mx,
+      paddingTop: py,
+      paddingBottom: py,
+      paddingLeft: px,
+      paddingRight: px,
+      width: width,
+      height: height,
+    }}
+    className={styles.disabled}
+  >
+    {children}
+  </motion.button>
+)
