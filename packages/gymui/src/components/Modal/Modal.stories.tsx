661c5007445b683edffd27047860024650b0be08
jackHotch <jhotchkiss2021@gmail.com>
2024-08-23 14:26:14 -0400

    created modal component

---
+++ packages/gymui/src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Modal } from './Modal'
+import { ComponentProps } from 'react'
+import { fn } from '@storybook/test'
+
+type StoryProps = ComponentProps<typeof Modal>
+
+const meta: Meta<StoryProps> = {
+  component: Modal,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<StoryProps>
+
+export const FullPage: Story = {
+  render: (args) => {
+    return <Modal {...args} />
+  },
+}
