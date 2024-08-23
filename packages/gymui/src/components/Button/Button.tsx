a0809f3d2ba36999a46d7f064ee1ef806760217e
jackHotch <jhotchkiss2021@gmail.com>
2024-08-23 16:16:59 -0400

    put each sub component in their own files

--- packages/gymui/src/components/Button/Button.tsx
+++ packages/gymui/src/components/Button/Button.tsx
@@ -1,8 +1,11 @@
 import { CSSProperties } from 'react'
-import styles from './Button.module.css'
-import { HTMLMotionProps, motion } from 'framer-motion'
+import { HTMLMotionProps } from 'framer-motion'
+import { Primary } from './Primary'
+import { Secondary } from './Secondary'
+import { Danger } from './Danger'
+import { Disabled } from './Disabled'
 
-interface ButtonProps extends HTMLMotionProps<'button'> {
+export interface ButtonProps extends HTMLMotionProps<'button'> {
   sx?: CSSProperties
   children?: string
   mx?: string
@@ -13,90 +16,13 @@ interface ButtonProps extends HTMLMotionProps<'button'> {
   height?: string
 }
 
-const SCALEHOVER = 1.05
-const SCALETAP = 0.95
-const DURATION = 0.15
+export const SCALEHOVER = 1.05
+export const SCALETAP = 0.95
+export const DURATION = 0.15
 
 export const Button = () => <></>
 
-const Primary = ({
-  children = 'Primary',
-  sx,
-  my,
-  mx,
-  py,
-  px,
-  width,
-  height,
-  ...props
-}: ButtonProps) => (
-  <motion.button
-    {...props}
-    style={{
-      ...sx,
-      marginTop: my,
-      marginBottom: my,
-      marginLeft: mx,
-      marginRight: mx,
-      paddingTop: py,
-      paddingBottom: py,
-      paddingLeft: px,
-      paddingRight: px,
-      width: width,
-      height: height,
-    }}
-    className={styles.primary}
-    whileHover={{
-      scale: SCALEHOVER,
-      boxShadow: 'rgba(150, 150, 150, 0.5) 0px 2px 8px 2px',
-    }}
-    transition={{ duration: DURATION }}
-    whileTap={{ scale: SCALETAP }}
-  >
-    {children}
-  </motion.button>
-)
 Button.Primary = Primary
-
-const Secondary = ({ sx, children = 'Secondary', ...props }: ButtonProps) => (
-  <motion.button
-    {...props}
-    style={sx}
-    className={styles.secondary}
-    whileHover={{
-      scale: SCALEHOVER,
-      backgroundColor: 'var(--blue)',
-      color: 'white',
-      boxShadow: 'rgba(150, 150, 150, 0.5) 0px 2px 8px 2px',
-    }}
-    transition={{ duration: DURATION }}
-    whileTap={{ scale: SCALETAP }}
-  >
-    {children}
-  </motion.button>
-)
 Button.Secondary = Secondary
-
-const Danger = ({ sx, children = 'Danger', ...props }: ButtonProps) => (
-  <motion.button
-    {...props}
-    style={sx}
-    className={styles.danger}
-    whileHover={{
-      scale: SCALEHOVER,
-      boxShadow: 'rgba(150, 150, 150, 0.5) 0px 2px 8px 2px',
-    }}
-    transition={{ duration: DURATION }}
-    whileTap={{ scale: SCALETAP }}
-  >
-    {children}
-  </motion.button>
-)
 Button.Danger = Danger
-
-const Disabled = ({ sx, children = 'Danger', ...props }: ButtonProps) => (
-  <motion.button {...props} disabled style={sx} className={styles.disabled}>
-    {children}
-  </motion.button>
-)
 Button.Disabled = Disabled
