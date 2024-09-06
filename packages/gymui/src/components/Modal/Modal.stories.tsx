ebd1de65df277801322a9d6856232c1c1f5ec74f
jackHotch <jhotchkiss2021@gmail.com>
2024-09-06 17:07:21 -0400

    added motion to modal

--- packages/gymui/src/components/Modal/Modal.stories.tsx
+++ packages/gymui/src/components/Modal/Modal.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import { Modal } from './Modal'
 import { ComponentProps } from 'react'
 import { fn } from '@storybook/test'
+import { AnimatePresence } from 'framer-motion'
 
 type StoryProps = ComponentProps<typeof Modal>
 
@@ -21,6 +22,7 @@ const meta: Meta<StoryProps> = {
     },
   },
   args: {
+    visible: true,
     width: '400px',
     height: '400px',
     sx: {},
@@ -34,14 +36,18 @@ type Story = StoryObj<StoryProps>
 
 export const FullPage: Story = {
   args: {},
-  render: (args) => {
+  render: (args: any) => {
     return (
       <div style={{ textAlign: 'center' }}>
         <h1>This is the Text Behind the Modal</h1>
-        <Modal.FullPage {...args}>
-          <h2>This is a Full Page Modal</h2>
-          <p>This is Text</p>
-        </Modal.FullPage>
+        <AnimatePresence>
+          {args.visible && (
+            <Modal.FullPage {...args}>
+              <h2>This is a Full Page Modal</h2>
+              <p>This is Text</p>
+            </Modal.FullPage>
+          )}
+        </AnimatePresence>
       </div>
     )
   },
