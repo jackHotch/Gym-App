a1015df31b14afaadb008be268ca5a27744e0e6b
jackHotch <jhotchkiss2021@gmail.com>
2024-09-06 00:49:13 -0400

    finished the full page modal

--- packages/gymui/src/components/Modal/Modal.tsx
+++ packages/gymui/src/components/Modal/Modal.tsx
@@ -1,10 +1,13 @@
-import { CSSProperties, ReactElement } from 'react'
+import { CSSProperties, ReactElement, ReactNode } from 'react'
 import { HTMLMotionProps } from 'framer-motion'
 import { FullPage } from './FullPage'
 
 export interface ModalProps extends HTMLMotionProps<'div'> {
-  children?: ReactElement
+  children?: ReactNode
   sx?: CSSProperties
+  width?: string
+  height?: string
+  onOutsideClick?: () => void
 }
 
 export const Modal = () => <></>
