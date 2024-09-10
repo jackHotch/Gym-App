a1c2429f61e4886669a4c728bae18385a45e0976
jackHotch <jhotchkiss2021@gmail.com>
2024-09-09 21:57:20 -0400

    created block skeleton component

---
+++ packages/gymui/src/components/Skeleton/Block.tsx
@@ -0,0 +1,31 @@
+import { CSSProperties } from 'react'
+import styles from './Skeleton.module.css'
+import { motion } from 'framer-motion'
+
+export interface BlockProps {
+  sx?: CSSProperties
+  count?: number
+  width?: string
+  height?: string
+}
+
+export const Block = ({ sx, count = 1, width, height = '24px' }: BlockProps) => {
+  return (
+    <>
+      {[...Array(count)].map((_, key) => (
+        <motion.div
+          key={key}
+          className={styles.line}
+          style={{ width: width, height: height, ...sx }}
+          animate={{ backgroundColor: 'var(--darkgrey)' }}
+          transition={{
+            repeat: Infinity,
+            duration: 1,
+            repeatType: 'reverse',
+            delay: key * 0.1,
+          }}
+        />
+      ))}
+    </>
+  )
+}
