a291262db5e6c2ea3854cf7128fcce6322b2a03d
jackHotch <jhotchkiss2021@gmail.com>
2024-09-21 19:28:27 -0400

    refactored /database files to use postgres

--- apps/api/src/database/Weight.ts
+++ apps/api/src/database/Weight.ts
@@ -1,41 +1,35 @@
-import mysql from 'mysql2'
+import pg from 'pg'
+const { Pool } = pg
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-const pool = mysql
-  .createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-  })
-  .promise()
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  port: parseInt(process.env.POSTGRES_PORT),
+})
+
+pool.connect().then(() => {
+  console.log('COnnected!')
+})
 
 export async function getAllWeight() {
-  const [rows] = await pool.query('SELECT * FROM weight')
+  const { rows } = await pool.query(`SELECT * FROM weight`)
   return rows
 }
 
 export async function getWeight(id: string) {
-  const [row] = await pool.query(
-    `
-    SELECT * 
-    FROM weight
-    WHERE id = ?
-    `,
-    [id]
-  )
-  return row
+  const { rows } = await pool.query(`SELECT * FROM weight WHERE id = $1`, [id])
+  return rows[0]
 }
 
 export async function createEntry(weight: number, date: string) {
-  await pool.query('INSERT INTO `weight` (`weight`, `date`) VALUES (?, ?)', [
-    weight,
-    date,
-  ])
+  await pool.query(`INSERT INTO weight (weight, date) VALUES ($1, $2)`, [weight, date])
 }
 
 export async function deleteEntry(id: string) {
-  await pool.query('DELETE FROM `weight` WHERE id = ?', [id])
+  await pool.query(`DELETE FROM weight WHERE id = $1`, [id])
 }
