24c8c744382cd9e7216007e37a2429d647fd4ec3
jackHotch <jhotchkiss2021@gmail.com>
2024-09-25 22:22:28 -0400

    added parsing files

---
+++ DatabasePG/ExerciseParser/index.js
@@ -0,0 +1,36 @@
+const pg = require('pg')
+const { Pool } = pg
+const fs = require('fs')
+const { parse } = require('csv-parse')
+
+const rows = new Set()
+
+fs.createReadStream('strong.csv')
+  .pipe(parse({ delimiter: ',', from_line: 2 }))
+  .on('data', function (row) {
+    rows.add(row[3])
+  })
+  .on('error', function (error) {
+    console.log(error.message)
+  })
+  .on('end', async function () {
+    await insertIntoMySql(rows)
+    console.log('finished parsing')
+  })
+
+async function insertIntoMySql(rows) {
+  const pool = new Pool({
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'gymapp',
+    user: 'postgres',
+    password: '12345',
+  })
+
+  pool.connect()
+
+  for (const name of rows) {
+    await pool.query(`insert into exercises (name) values ('${name}')`)
+  }
+  console.log('All done')
+}
