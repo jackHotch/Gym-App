f62eb9ea96b1dfc99f94a12acff791267b76fca9
jackHotch <jhotchkiss2021@gmail.com>
2024-09-27 17:55:37 -0400

    updated /database files

--- apps/api/src/database/Splits.ts
+++ apps/api/src/database/Splits.ts
@@ -1,27 +1,25 @@
-import mysql from 'mysql2'
+import pg from 'pg'
+const { Pool } = pg
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-const pool = mysql
-  .createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-  })
-  .promise()
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  port: parseInt(process.env.POSTGRES_PORT),
+})
+
+pool.connect()
 
 export async function getAllSplits() {
-  const [rows] = await pool.query(`SELECT * FROM splits`)
+  const { rows } = await pool.query(`SELECT * FROM splits`)
   return rows
 }
 
 export async function getCurrentSplit() {
-  const [row] = await pool.query(`
-    SELECT name
-    FROM splits
-    WHERE active = 1
-    `)
-  return row[0]
+  const { rows } = await pool.query(`SELECT name FROM splits WHERE active = 1`)
+  return rows[0]
 }
