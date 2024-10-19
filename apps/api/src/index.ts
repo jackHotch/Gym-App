b1d662c9c0100d34df99b96294f7412a678ef44d
jackHotch <jhotchkiss2021@gmail.com>
2024-10-18 21:21:53 -0400

    updated root endpoint to match resume branch

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello!' })
+  res.json({ message: 'gymapp/api' })
 })
 
 app.get('/cronjob', (req, res) => {
