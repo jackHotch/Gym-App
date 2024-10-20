c07c90a25e719a37c69ad1a233972ec65b931bdd
jackHotch <jhotchkiss2021@gmail.com>
2024-10-19 21:27:26 -0400

    finished Calendar component in gymui

---
+++ packages/gymui/src/components/Calendar/Calendar.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Calendar } from './Calendar'
+import { ComponentProps } from 'react'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { fn } from '@storybook/test'
+
+type StoryProps = ComponentProps<typeof Calendar>
+
+const meta: Meta<StoryProps> = {
+  component: Calendar,
+  tags: ['autodocs'],
+  args: {
+    sx: {
+      margin: '20px',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<StoryProps>
+
+export const Primary: Story = {
+  args: {},
+  render: (args) => {
+    return (
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Calendar {...args} />
+      </LocalizationProvider>
+    )
+  },
+}
