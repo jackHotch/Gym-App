fe3f03b9f244161f5ac669b61d48674e46db44e5
jackHotch <jhotchkiss2021@gmail.com>
2024-10-22 18:24:40 -0400

    removed test code

--- apps/gym/src/app/progress/page.tsx
+++ apps/gym/src/app/progress/page.tsx
@@ -1,18 +1,5 @@
-'use client'
-
-import { DatePicker } from '@/components/reusable'
-import { convertDate } from '@/utils/utils'
-import dayjs from 'dayjs'
-import { useState } from 'react'
-
 const Progress = () => {
-  const [date, setDate] = useState(dayjs())
-
-  return (
-    <div style={{ margin: '200px', width: 'max-content' }}>
-      <DatePicker value={date} setDate={setDate} />
-    </div>
-  )
+  return <div>Progress</div>
 }
 
 export default Progress
--- apps/gym/src/components/reusable/DatePicker/DatePicker.test.tsx
+++ apps/gym/src/components/reusable/DatePicker/DatePicker.test.tsx
@@ -2,8 +2,6 @@ import { render } from '@testing-library/react'
 import { DatePicker } from './DatePicker'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { convertDate } from '@/utils/utils'
-import { useState } from 'react'
 import dayjs from 'dayjs'
 
 describe('DatePicker', () => {
