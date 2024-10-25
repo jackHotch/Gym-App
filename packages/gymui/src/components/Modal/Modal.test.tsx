e458ec0b4b3a0c635c6631750313008b84e28b9b
jackHotch <jhotchkiss2021@gmail.com>
2024-10-25 18:16:49 -0400

    added tests

--- packages/gymui/src/components/Modal/Modal.test.tsx
+++ packages/gymui/src/components/Modal/Modal.test.tsx
@@ -14,4 +14,27 @@ describe('Modal', () => {
     expect(baseElement).toBeTruthy()
     expect(modalBackgroundClick).toHaveBeenCalled()
   })
+
+  it('Modal.Popover should render successfully', () => {
+    const { baseElement } = render(
+      <Modal.Popover>
+        <p>Popover Modal</p>
+      </Modal.Popover>
+    )
+
+    expect(baseElement).toBeTruthy()
+  })
+
+  it('Modal.Item should render successfully', () => {
+    const itemClick = jest.fn()
+    const { baseElement, getByText } = render(
+      <Modal.Popover>
+        <Modal.Item onClick={itemClick}>Item</Modal.Item>
+      </Modal.Popover>
+    )
+    fireEvent.click(getByText('Item'))
+
+    expect(baseElement).toBeTruthy()
+    expect(itemClick).toHaveBeenCalled()
+  })
 })
