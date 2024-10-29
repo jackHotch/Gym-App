16c4cea9648916e3c7936750f7923d5697aaa227
jackHotch <jhotchkiss2021@gmail.com>
2024-10-29 19:23:40 -0400

    added animation when DatePickers appear

--- apps/gym/src/components/reusable/DateRangePicker/DateRangePicker.tsx
+++ apps/gym/src/components/reusable/DateRangePicker/DateRangePicker.tsx
@@ -6,9 +6,31 @@ import { DatePicker } from '../DatePicker'
 import { RangeSelector } from '@/components/isolated/Weight'
 import { useState } from 'react'
 import { useToggle } from '@/hooks'
+import { motion, AnimatePresence, LayoutGroup } from 'framer-motion'
 
 interface DateRangePickerProps {
   updateChart: (startDate?: Dayjs, endDate?: Dayjs) => void
+}
+
+const motionVariants = {
+  hidden: {
+    x: '20%',
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 60,
+      damping: 10,
+      duration: 1,
+    },
+  },
+  exit: {
+    x: '20%',
+    opacity: 0,
+  },
 }
 
 export const DateRangePicker = ({ updateChart }: DateRangePickerProps) => {
@@ -17,18 +39,27 @@ export const DateRangePicker = ({ updateChart }: DateRangePickerProps) => {
   const [isCustom, , openDatePickers, closeDatePickers] = useToggle()
 
   return (
-    <div className={styles.container}>
-      <RangeSelector
-        updateChart={updateChart}
-        openDatePickers={openDatePickers}
-        closeDatePickers={closeDatePickers}
-      />
-      {isCustom && (
-        <div className={styles.date_picker_container}>
-          <DatePicker value={startDate} setDate={setStartDate} />
-          <DatePicker value={endDate} setDate={setEndDate} />
-        </div>
-      )}
-    </div>
+    <LayoutGroup>
+      <motion.div className={styles.container}>
+        <motion.div key={1} layout='position'>
+          <RangeSelector
+            updateChart={updateChart}
+            openDatePickers={openDatePickers}
+            closeDatePickers={closeDatePickers}
+          />
+        </motion.div>
+
+        <motion.div style={{ height: '100%' }} layout='position'>
+          <AnimatePresence>
+            {isCustom && (
+              <div key={2} className={styles.date_picker_container}>
+                <DatePicker value={startDate} setDate={setStartDate} />
+                <DatePicker value={endDate} setDate={setEndDate} />
+              </div>
+            )}
+          </AnimatePresence>
+        </motion.div>
+      </motion.div>
+    </LayoutGroup>
   )
 }
