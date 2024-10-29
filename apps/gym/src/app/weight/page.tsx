9de385486ca7f223ab5ea57dac97c85886ff232c
jackHotch <jhotchkiss2021@gmail.com>
2024-10-28 22:13:32 -0400

    finished filter function

--- apps/gym/src/app/weight/page.tsx
+++ apps/gym/src/app/weight/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { useWeight } from '@/hooks'
 import styles from './Weight.module.css'
 import { WeightChart, WeightList } from '@/components/isolated/Weight'
+import { getChartData } from '@/utils/utils'
 
 const Weight = () => {
   return (
