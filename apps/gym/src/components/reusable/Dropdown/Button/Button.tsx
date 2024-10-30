f285e5a0210a755785883badda94e468dbd9494b
jackHotch <jhotchkiss2021@gmail.com>
2024-10-30 18:35:04 -0400

    added animation to arrow in RangeSelector

--- apps/gym/src/components/reusable/Dropdown/Button/Button.tsx
+++ apps/gym/src/components/reusable/Dropdown/Button/Button.tsx
@@ -1,11 +1,17 @@
 import styles from './Button.module.css'
 import { DropdownProps } from '../Dropdown'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { forwardRef, Ref } from 'react'
 
-export const Button = ({ sx, children, ...props }: DropdownProps) => {
-  return (
-    <div className={styles.container} style={sx} {...props}>
-      {children} <ExpandMoreIcon />
-    </div>
-  )
-}
+type reactRef = Ref<SVGSVGElement>
+
+export const Button = forwardRef(
+  ({ sx, children, ...props }: DropdownProps, ref: reactRef) => {
+    return (
+      <div {...props} className={styles.container} style={sx}>
+        {children}
+        <ExpandMoreIcon ref={ref} />
+      </div>
+    )
+  }
+)
