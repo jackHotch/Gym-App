0d01d756d032176468424eb1583797ec48905351
jackHotch <jhotchkiss2021@gmail.com>
2024-11-08 20:18:35 -0500

    worked on logo and profile

---
+++ apps/gym/src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+import styles from './Profile.module.css'
+
+const Profile = () => {
+  return <div className={styles.container}>Profile Page</div>
+}
+
+export default Profile
--- packages/gymui/src/components/TabList/Option/Option.tsx
+++ packages/gymui/src/components/TabList/Option/Option.tsx
@@ -1,8 +1,14 @@
+import styles from './Option.module.css'
+
 export interface OptionProps {
   name: string
   href: () => void
 }
 
 export const Option = ({ name, href }: OptionProps) => {
-  return <div onClick={href}>{name}</div>
+  return (
+    <div className={styles.container} onClick={href}>
+      {name}
+    </div>
+  )
 }
