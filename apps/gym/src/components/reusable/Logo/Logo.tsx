5ec59d37517f31993d2fb22d31616a2464ec2ec8
jackHotch <jhotchkiss2021@gmail.com>
2024-11-15 16:47:07 -0500

    updated logo

--- apps/gym/src/components/reusable/Logo/Logo.tsx
+++ apps/gym/src/components/reusable/Logo/Logo.tsx
@@ -1,13 +1,12 @@
 import styles from './Logo.module.css'
-import Image from 'next/image'
 import { NavOptions } from '@/constants'
 import Link from 'next/link'
 
 export const Logo = () => {
   return (
     <Link key={1} className={styles.container} href={NavOptions[0].path}>
-      <Image src='/images/dumbbell-blue.png' alt='logo' width={25} height={25} />
-      <span>Gym App</span>
+      <img src='/images/dumbbell-blue.png' alt='logo' width={25} height={25} />
+      <span>JACKED</span>
     </Link>
   )
 }
