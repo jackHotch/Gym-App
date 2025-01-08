84454337739ce1aae3b42f738da04f8dd75bef70
jackHotch <jhotchkiss2021@gmail.com>
2025-01-08 17:50:21 -0500

    set up mobile header

---
+++ apps/gym/src/components/reusable/Header/HeaderMobile/DropdownMobile/DropdownMobile.tsx
@@ -0,0 +1,6 @@
+import styles from './DropdownMobile.module.css'
+import { DropdownMobileProps } from '@/types'
+
+export const DropdownMobile = ({}: DropdownMobileProps) => {
+  return <div className={styles.container}>DropdownMobile</div>
+}
