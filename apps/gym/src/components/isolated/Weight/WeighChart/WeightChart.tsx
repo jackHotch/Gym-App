3808df61a0867438d1d39055c723d9ac95f13b46
jackHotch <jhotchkiss2021@gmail.com>
2025-01-24 19:42:29 -0500

    fixed css and started working on mobile chart

--- apps/gym/src/components/isolated/Weight/WeighChart/WeightChart.tsx
+++ apps/gym/src/components/isolated/Weight/WeighChart/WeightChart.tsx
@@ -1,12 +1,53 @@
 import styles from './WeightChart.module.css'
+import { useWeight } from '@/hooks/api/useWeight'
+import { getChartData } from '@/utils/utils'
+import dayjs, { Dayjs } from 'dayjs'
+import { useState, useEffect } from 'react'
 import { WeightChartDesktop } from './WeightChartDesktop/WeightChartDesktop'
 import { WeightChartMobile } from './WeightChartMobile/WeightChartMobile'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import isBetween from 'dayjs/plugin/isBetween'
+dayjs.extend(isBetween)
 
 export const WeightChart = () => {
+  const { data, isLoading } = useWeight()
+  const [labels, setLabels] = useState([])
+  const [weightData, setWeightData] = useState([])
+
+  useEffect(() => {
+    const [l, d] = getChartData(data)
+    setLabels(l)
+    setWeightData(d)
+  }, [data])
+
+  const filterByRange = (startDate?: Dayjs, endDate?: Dayjs) => {
+    startDate = startDate ? startDate : dayjs(data[0].date)
+    endDate = endDate ? endDate : dayjs(data[data.length - 1].date)
+    const filteredData = data?.filter((value) => {
+      return dayjs(value.date).isBetween(startDate, endDate, 'day', '[]')
+    })
+    const [l, d] = getChartData(filteredData)
+    setLabels(l)
+    setWeightData(d)
+  }
+
   return (
-    <>
-      <WeightChartDesktop />
-      <WeightChartMobile />
-    </>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <WeightChartDesktop
+        filter={filterByRange}
+        weightData={weightData}
+        labels={labels}
+        isLoading={isLoading}
+        data={data}
+      />
+      <WeightChartMobile
+        filter={filterByRange}
+        weightData={weightData}
+        labels={labels}
+        isLoading={isLoading}
+        data={data}
+      />
+    </LocalizationProvider>
   )
 }
--- apps/gym/src/components/isolated/Weight/WeighChart/WeightChartMobile/WeightChartMobile.tsx
+++ apps/gym/src/components/isolated/Weight/WeighChart/WeightChartMobile/WeightChartMobile.tsx
@@ -1,6 +1,23 @@
 import styles from './WeightChartMobile.module.css'
-import { WeightChartMobileProps } from '@/types'
+import { WeightChartDesktopProps } from '@/types'
+import { ChartHeader } from './ChartHeader/ChartHeader'
+import { Chart, DateRangePicker } from '@/components/reusable'
 
-export const WeightChartMobile = ({}: WeightChartMobileProps) => {
-  return <div className={styles.container}>WeightChartMobile</div>
+export const WeightChartMobile = ({
+  filter,
+  weightData,
+  labels,
+  isLoading,
+  data,
+}: WeightChartDesktopProps) => {
+  return (
+    <div className={styles.container}>
+      <ChartHeader
+        startingWeight={weightData[0]}
+        endingWeight={weightData[weightData.length - 1]}
+      />
+      <DateRangePicker filter={filter} data={data} />
+      <Chart labels={labels} data={weightData} isLoading={isLoading} height='250px' />
+    </div>
+  )
 }
