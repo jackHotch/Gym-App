eff7b2867e74aa85adff7b9dc820678d12a3f0ab
jackHotch <jhotchkiss2021@gmail.com>
2025-01-31 17:53:56 -0500

    finished css for weightlist

--- apps/gym/src/types/types.ts
+++ apps/gym/src/types/types.ts
@@ -140,7 +140,7 @@ export interface RangeSelectorProps {
 export interface WeightListEntryProps {
   value: IWeightData
   id: number
-  difference: string
+  difference: number
 }
 
 export interface ExerciseProps {
