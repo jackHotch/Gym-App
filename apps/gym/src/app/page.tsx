48e8089af817d5a48d92123f07a31785147e53dd
jackHotch <jhotchkiss2021@gmail.com>
2025-01-31 18:57:11 -0500

    modified message on home page

--- apps/gym/src/app/page.tsx
+++ apps/gym/src/app/page.tsx
@@ -14,14 +14,15 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <h1>
-        Jack Hotchkiss's Gym Tracking App{' '}
+        Jack Hotchkiss's Gym Tracking App
         <img src='/images/dumbbell-blue.png' alt='Dumbbell Icon' width={45} />
       </h1>
       <p className={styles.description}>
         Hi, this is a personal project I am currently working on. It will be a web app to
-        track my progress in the gym. Currently only the Record and Weight pages have
-        content. Please feel free to play around with this website and I have linked the
-        GitHub repository for this project below if you want to check it out.
+        track my progress in the gym. Currently the app is fully responsive and only the
+        Record and Weight pages have content. Please feel free to play around with this
+        website and I have linked the GitHub repository for this project below if you want
+        to check it out.
       </p>
       <p className={styles.thanks}>Thank You!</p>
       <div className={styles.buttons}>
