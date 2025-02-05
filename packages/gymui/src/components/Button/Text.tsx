4f794b10d1ec318fbe86bb2d09e55b4b7e876cb8
jackHotch <jhotchkiss2021@gmail.com>
2025-02-05 18:06:42 -0500

    fixed responsive issue on weight page

--- packages/gymui/src/components/Button/Text.tsx
+++ packages/gymui/src/components/Button/Text.tsx
@@ -3,7 +3,12 @@ import styles from './Button.module.css'
 import { motion } from 'motion/react'
 
 export const Text = ({ children, sx, ...props }: ButtonProps) => (
-  <motion.button {...props} style={sx} className={styles.text}>
+  <motion.button
+    {...props}
+    style={sx}
+    className={styles.text}
+    whileHover={{ textDecoration: 'underline' }}
+  >
     {children}
   </motion.button>
 )
