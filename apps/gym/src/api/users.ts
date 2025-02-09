10e3f85b9f68f970c675ae77d8d2da897c227702
jackHotch <jhotchkiss2021@gmail.com>
2025-02-09 18:27:07 -0500

    added client side form validation and sign up functionality

--- apps/gym/src/api/users.ts
+++ apps/gym/src/api/users.ts
@@ -1,25 +1,17 @@
 'use server'
 
 import axios from 'axios'
-import { loginFormData, signUpFormData } from '@/types'
 
 const URL = process.env.URL + '/auth'
 
-const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
-const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{5,24}$/
+import { signUpFormData, loginFormData } from '@/types'
 
-export const validateSignUpForm = (formData: signUpFormData) => {
-  if (!formData.firstName) return 'First Name is Required'
-  if (!formData.lastName) return 'Last Name is Required'
-  if (!emailRegex.test(formData.email)) return 'Invalid Email Format'
-  if (!passwordRegex.test(formData.password)) return 'Invalid Password'
-  return null
-}
-
-export const validateLoginForm = (formData: loginFormData) => {}
-
-export const signUpAction = (formData: signUpFormData) => {
-  validateSignUpForm(formData)
+export const signUpAction = async (formData: signUpFormData) => {
+  try {
+    await axios.post(`${URL}/signup`, formData)
+  } catch (error) {
+    return error.response.data.message
+  }
   return null
 }
 
--- apps/gym/src/app/(auth)/signup/page.tsx
+++ apps/gym/src/app/(auth)/signup/page.tsx
@@ -3,16 +3,19 @@
 import styles from './SignUp.module.css'
 import { Form } from '@gymapp/gymui/Form'
 import { Button } from '@gymapp/gymui/Button'
-import { FormEvent, TextInputChangeEvent } from '@/types'
 import { useState } from 'react'
-import { useAuth } from '@/hooks/api'
 import Image from 'next/image'
-import { validateSignUpForm } from '@/api/users'
+import { Error } from '@gymapp/gymui/Error'
+import { motion } from 'motion/react'
+import { signUpFormData, TextInputChangeEvent } from '@/types'
+import { useAuth } from '@/hooks/api'
 
 const SignUp = () => {
   const { signUpMutation } = useAuth()
-  const { mutate: signUp } = signUpMutation()
+  const { mutate: signUp, data: signUpError } = signUpMutation()
   const [error, setError] = useState('')
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+  const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{5,24}$/
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
@@ -26,54 +29,72 @@ const SignUp = () => {
     setFormData(temp)
   }
 
-  const handleSignUp = () => {
-    const error = validateSignUpForm(formData)
-    if (!error) return signUp(formData)
-    setError(error)
+  const validateSignUpForm = (formData: signUpFormData) => {
+    if (!formData.firstName) return 'First Name is Required'
+    if (!formData.lastName) return 'Last Name is Required'
+    if (!emailRegex.test(formData.email)) return 'Invalid Email Format'
+    if (!passwordRegex.test(formData.password)) return 'Invalid Password'
+    return null
+  }
+
+  const handleSignUp = async () => {
+    const formError = validateSignUpForm(formData)
+    if (formError) {
+      setError(formError)
+      return
+    }
+
+    signUp(formData, {
+      onSuccess: (errorMessage) => {
+        setError(errorMessage)
+      },
+    })
   }
 
   return (
     <div className={styles.container}>
-      <h1>Create Account</h1>
+      <motion.div layout='size' className={styles.card}>
+        <h1>Create Account</h1>
 
-      <div className={styles.input_container}>
-        <Form.Text.Outline
-          placeholder='First Name'
-          value={formData.firstName}
-          onChange={(e) => handleChange(e, 'firstName')}
-        />
-        <Form.Text.Outline
-          placeholder='Last Name'
-          value={formData.lastName}
-          onChange={(e) => handleChange(e, 'lastName')}
-        />
-        <Form.Text.Outline
-          placeholder='Email Address'
-          type='email'
-          value={formData.email}
-          onChange={(e) => handleChange(e, 'email')}
-        />
-        <Form.Text.Password
-          placeholder='Password'
-          type='password'
-          value={formData.password}
-          onChange={(e) => handleChange(e, 'password')}
-        />
-        <p>{error ? error : ''}</p>
-      </div>
+        <div className={styles.input_container}>
+          <Form.Text.Outline
+            placeholder='First Name'
+            value={formData.firstName}
+            onChange={(e) => handleChange(e, 'firstName')}
+          />
+          <Form.Text.Outline
+            placeholder='Last Name'
+            value={formData.lastName}
+            onChange={(e) => handleChange(e, 'lastName')}
+          />
+          <Form.Text.Outline
+            placeholder='Email Address'
+            type='email'
+            value={formData.email}
+            onChange={(e) => handleChange(e, 'email')}
+          />
+          <Form.Text.Password
+            placeholder='Password'
+            type='password'
+            value={formData.password}
+            onChange={(e) => handleChange(e, 'password')}
+          />
+          <Error isVisible={error ? true : false}>{error}</Error>
+        </div>
 
-      <div className={styles.button_container}>
-        <Button.Primary size='medium' sx={{ padding: '12px' }} onClick={handleSignUp}>
-          Create Account
-        </Button.Primary>
-        <button
-          className={styles.google_button}
-          onClick={() => alert('Not Implemented Yet')}
-        >
-          <Image src='/images/google.png' alt='google icon' width={20} height={20} />
-          Sign Up With Google
-        </button>
-      </div>
+        <div className={styles.button_container}>
+          <Button.Primary size='medium' sx={{ padding: '12px' }} onClick={handleSignUp}>
+            Create Account
+          </Button.Primary>
+          <button
+            className={styles.google_button}
+            onClick={() => alert('Not Implemented Yet')}
+          >
+            <Image src='/images/google.png' alt='Google Icon' width={20} height={20} />
+            Sign Up With Google
+          </button>
+        </div>
+      </motion.div>
     </div>
   )
 }
