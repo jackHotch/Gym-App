0b156e82b5a710f62d2f2deb767ae947c45787dd
jackHotch <jhotchkiss2021@gmail.com>
2025-02-08 20:07:02 -0500

    setup form validation

---
+++ apps/gym/src/api/users.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import axios from 'axios'
+import { loginFormData, signUpFormData } from '@/types'
+
+const URL = process.env.URL + '/auth'
+
+const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{5,24}$/
+
+export const validateSignUpForm = (formData: signUpFormData) => {
+  if (!formData.firstName) return { valid: false, message: 'First Name is Required' }
+  if (!formData.lastName) return { valid: false, message: 'Last Name is Required' }
+  if (!emailRegex.test(formData.email))
+    return { valid: false, message: 'Invalid Email Format' }
+  if (!passwordRegex.test(formData.password))
+    return { valid: false, message: 'Invalid Password' }
+  return { valid: true }
+}
+
+export const validateLoginForm = (formData: loginFormData) => {}
+
+export const signUpAction = (formData: signUpFormData) => {
+  return null
+}
+
+export const loginAction = (formData: loginFormData) => {
+  return null
+}
--- apps/gym/src/hooks/api/useExercises.ts
+++ apps/gym/src/hooks/api/useExercises.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { getExercises } from '@/api'
+import { getExercises } from '@/api/exercise'
 
 export const useExercises = () => {
   return useQuery({
