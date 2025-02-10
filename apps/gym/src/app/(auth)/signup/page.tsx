bb7d0f21f96f2d32560495fcbc0189ff90f1e425
jackHotch <jhotchkiss2021@gmail.com>
2025-02-09 22:31:15 -0500

    added redirection after sign up

--- apps/gym/src/app/(auth)/signup/page.tsx
+++ apps/gym/src/app/(auth)/signup/page.tsx
@@ -9,10 +9,12 @@ import { Error } from '@gymapp/gymui/Error'
 import { motion } from 'motion/react'
 import { signUpFormData, TextInputChangeEvent } from '@/types'
 import { useAuth } from '@/hooks/api'
+import { useRouter } from 'next/navigation'
 
 const SignUp = () => {
   const { signUpMutation } = useAuth()
   const { mutate: signUp, data: signUpError } = signUpMutation()
+  const router = useRouter()
   const [error, setError] = useState('')
   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
   const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{5,24}$/
@@ -46,7 +48,8 @@ const SignUp = () => {
 
     signUp(formData, {
       onSuccess: (errorMessage) => {
-        setError(errorMessage)
+        if (errorMessage) setError(errorMessage)
+        else router.push('/dashboard')
       },
     })
   }
