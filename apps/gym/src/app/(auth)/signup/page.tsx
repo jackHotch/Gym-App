2a2a4edddbc5970be4b5feb4db7a3154e7ed78c2
jackHotch <jhotchkiss2021@gmail.com>
2025-02-20 22:13:30 -0500

    fixed feature flag hook and added auth flag

--- apps/gym/src/app/(auth)/signup/page.tsx
+++ apps/gym/src/app/(auth)/signup/page.tsx
@@ -8,11 +8,12 @@ import Image from 'next/image'
 import { Error } from '@gymapp/gymui/Error'
 import { motion } from 'motion/react'
 import { signUpFormData, TextInputChangeEvent } from '@/types'
-import { useAuth } from '@/hooks/api'
+import { useAuth, useFeatureFlag } from '@/hooks/api'
 import { useRouter } from 'next/navigation'
 import { emailRegex, passwordRegex } from '@/constants'
 
 const SignUp = () => {
+  const { data: authEnabled } = useFeatureFlag('Auth_Functionality')
   const { signUpMutation } = useAuth()
   const { mutate: signUp } = signUpMutation()
   const router = useRouter()
@@ -45,10 +46,10 @@ const SignUp = () => {
       return
     } else {
       setError('')
-      if (process.env.NODE_ENV != 'development') alert('Not Implemented Yet')
+      if (authEnabled == false) alert('Not Implemented Yet')
     }
 
-    if (process.env.NODE_ENV == 'development') {
+    if (authEnabled) {
       signUp(formData, {
         onSuccess: (errorMessage) => {
           if (errorMessage) setError(errorMessage)
