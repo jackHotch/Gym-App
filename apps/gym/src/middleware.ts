47afc4b5c46a08643c35451d1b2406f695dff489
jackHotch <jhotchkiss2021@gmail.com>
2025-03-26 21:15:57 -0400

    tried to fix it, didn't

--- apps/gym/src/middleware.ts
+++ apps/gym/src/middleware.ts
@@ -14,6 +14,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!^$|^login$|^signup$|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|woff|woff2|ttf|otf)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|woff|woff2|ttf|otf)$).*)',
   ],
 }
