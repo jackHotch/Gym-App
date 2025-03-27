37042048a67fa261864834b281a456e4ebed3c3c
jackHotch <jhotchkiss2021@gmail.com>
2025-03-26 21:15:57 -0400

    tried to fix it, didn't

--- apps/gym/src/app/(user)/dashboard/page.tsx
+++ apps/gym/src/app/(user)/dashboard/page.tsx
@@ -1,7 +1,11 @@
 import styles from './Dashboard.module.css'
 
 const Dashboard = () => {
-  return <div className={styles.container}>Dashboard</div>
+  return (
+    <div className={styles.container}>
+      Dashboard<p>{}</p>
+    </div>
+  )
 }
 
 export default Dashboard
--- apps/gym/src/utils/supabase/middleware.ts
+++ apps/gym/src/utils/supabase/middleware.ts
@@ -42,7 +42,8 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/') &&
+    !request.nextUrl.pathname.startsWith('/signup')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
