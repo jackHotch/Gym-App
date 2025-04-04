ce1e5239b6416d3eb3722313721d5f98a21da811
jackHotch <jhotchkiss2021@gmail.com>
2025-04-03 21:28:54 -0400

    updated splits endpoints

--- apps/api/src/database/Splits.ts
+++ apps/api/src/database/Splits.ts
@@ -9,12 +9,20 @@ const pool = new Pool({
 })
 pool.connect()
 
-export async function getAllSplits() {
+export async function cronjob() {
   const { rows } = await pool.query(`SELECT * FROM splits`)
   return rows
 }
 
-export async function getCurrentSplit() {
-  const { rows } = await pool.query(`SELECT name FROM splits WHERE active = 1`)
+export async function getAllSplits(userId: string) {
+  const { rows } = await pool.query(`SELECT * FROM splits WHERE user_id = $1`, [userId])
+  return rows
+}
+
+export async function getCurrentSplit(userId: string) {
+  const { rows } = await pool.query(
+    `SELECT name FROM splits WHERE active = 1 AND user_id = $1`,
+    [userId]
+  )
   return rows
 }
