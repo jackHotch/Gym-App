246b34e66461f1201f99d11847be768163b20c30
jackHotch <jhotchkiss2021@gmail.com>
2025-04-04 21:34:05 -0400

    WIP - working on updated server actions in frontend

--- apps/gym/src/actions/splits.ts
+++ apps/gym/src/actions/splits.ts
@@ -1,11 +1,19 @@
 'use server'
 
 import { ICurrentSplit } from '@/types'
+import { getSupabaseUserId } from '@/utils/supabase/getSupabaseUserId'
 import axios from 'axios'
 
-const URL = process.env.URL + '/api/splits'
+const URL = process.env.URL + '/splits'
 
 export const getCurrentSplit = async () => {
-  const { data } = await axios.get(`${URL}/current`)
+  const userId = await getSupabaseUserId()
+  console.log('!!!!!Jack: ' + userId)
+  const { data } = await axios.get(`${URL}/current`, {
+    params: {
+      userId: userId,
+    },
+  })
+  console.log('!!!!!JACK ' + data)
   return data as ICurrentSplit
 }
