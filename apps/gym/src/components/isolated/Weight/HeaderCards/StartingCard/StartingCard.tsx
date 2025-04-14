e7b57fb0f595c3f30520246b042eac44e607139c
jackHotch <jhotchkiss2021@gmail.com>
2025-04-13 22:34:29 -0400

    updated mobile loading states for no data

--- apps/gym/src/components/isolated/Weight/HeaderCards/StartingCard/StartingCard.tsx
+++ apps/gym/src/components/isolated/Weight/HeaderCards/StartingCard/StartingCard.tsx
@@ -2,15 +2,16 @@ import styles from '../HeaderCards.module.css'
 import { StartingCardProps } from '@/types'
 import { TailSpin } from 'react-loader-spinner'
 
-export const StartingCard = ({ weight, gainedWeight }: StartingCardProps) => {
+export const StartingCard = ({ weight, isEmpty }: StartingCardProps) => {
   return (
-    <div
-      className={styles.container}
-      // style={{
-      //   border: gainedWeight ? '1px solid var(--green) ' : '1px solid var(--red)',
-      // }}
-    >
-      {weight ? weight : <TailSpin color='var(--blue)' width='50' height='20' />}
+    <div className={styles.container}>
+      {weight ? (
+        weight
+      ) : isEmpty ? (
+        '-'
+      ) : (
+        <TailSpin color='var(--blue)' width='50' height='20' />
+      )}
       <span className={styles.label}>Start</span>
     </div>
   )
