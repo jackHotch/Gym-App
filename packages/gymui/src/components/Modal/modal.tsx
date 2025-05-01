c09eea06dea9480eea62c3879f0616cd156990b1
jackHotch <jhotchkiss2021@gmail.com>
2025-05-01 00:09:14 -0400

    added animations

--- packages/gymui/src/components/Modal/modal.tsx
+++ packages/gymui/src/components/Modal/modal.tsx
@@ -8,27 +8,7 @@ import { Title } from './modal-title'
 import { Description } from './modal-description'
 import { Footer } from './modal-footer'
 
-const modalVariants = {
-  hidden: {
-    scale: 0,
-  },
-  visible: {
-    scale: 1,
-    transition: {
-      type: 'spring',
-      stiffness: 100,
-      damping: 16,
-    },
-  },
-  exit: {
-    scale: 0,
-    transition: {
-      duration: 0.2,
-    },
-  },
-}
-
-export interface ModalProps extends HTMLMotionProps<'div'> {
+export interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
   open: boolean
   onOpenChange: (open: boolean) => void
   children: React.ReactNode | React.ReactNode[]
@@ -38,9 +18,7 @@ export interface ModalProps extends HTMLMotionProps<'div'> {
 export const Modal = ({ open, onOpenChange, children, sx, ...props }: ModalProps) => {
   return (
     <ModalContext.Provider value={{ open, onOpenChange }}>
-      <motion.div data-testid='background' {...props}>
-        {children}
-      </motion.div>
+      <div {...props}>{children}</div>
     </ModalContext.Provider>
   )
 }
