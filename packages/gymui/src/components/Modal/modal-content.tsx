6e73e6fee4d51374cb96c5e63c4742a1373a002e
jackHotch <jhotchkiss2021@gmail.com>
2025-05-01 00:09:14 -0400

    added animations

--- packages/gymui/src/components/Modal/modal-content.tsx
+++ packages/gymui/src/components/Modal/modal-content.tsx
@@ -1,26 +1,52 @@
 import styles from './modal.module.css'
 import { useModalContext } from './modal-context'
+import { AnimatePresence, motion } from 'motion/react'
 
 export interface ContentProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode
   sx?: React.CSSProperties
 }
 
+const modalVariants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.8,
+  },
+}
+
 export function Content({ children, sx, ...props }: ContentProps) {
   const { open, onOpenChange } = useModalContext()
 
-  if (!open) return null
-
   return (
-    <div
-      style={sx}
-      className={styles.background}
-      onClick={() => onOpenChange(false)}
-      {...props}
-    >
-      <div className={styles.content_container} onClick={(e) => e.stopPropagation()}>
-        {children}
-      </div>
-    </div>
+    <AnimatePresence>
+      {open && (
+        <div
+          style={sx}
+          className={styles.background}
+          onClick={() => onOpenChange(false)}
+          {...props}
+        >
+          <motion.div
+            className={styles.content_container}
+            onClick={(e) => e.stopPropagation()}
+            variants={modalVariants}
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+            transition={{ duration: 0.2 }}
+          >
+            {children}
+          </motion.div>
+        </div>
+      )}
+    </AnimatePresence>
   )
 }
