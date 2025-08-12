584df922137c8df38301f16f912560ed72cb80e9
jackHotch <jhotchkiss2021@gmail.com>
2025-08-11 21:15:14 -0400

    added zod schema to middleware

--- apps/api/src/middleware/middleware.ts
+++ apps/api/src/middleware/middleware.ts
@@ -1,9 +1,22 @@
 import { NextFunction, Request, Response } from 'express'
 import { formatResponse } from '../utils/utils'
+import * as z from 'zod'
 
 export const userIdMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  if (req.query.userId) {
-    next()
+  const userId = req.query.userId
+
+  if (userId) {
+    const userIdSchema = z.string().trim().min(1)
+    const userIdValid = userIdSchema.safeParse({ userId })
+
+    if (userIdValid) {
+      next()
+    } else {
+      const { statusCode, ...response } = formatResponse(400, {
+        message: 'UserID is not valid',
+      })
+      res.status(statusCode).json({ ...response })
+    }
   } else {
     const { statusCode, ...response } = formatResponse(400, {
       message: 'UserID is required',
